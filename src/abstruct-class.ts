3a0ec048b61fe06734a3d95ad8bd7bba1123d5ea
takashiono2 <takashiono710@gmail.com>
2024-01-21 01:30:10 +0900

    section8

---
+++ src/abstruct-class.ts
@@ -0,0 +1,16 @@
+export {};
+
+abstract class Animal{
+  abstract cry(): string;
+}
+
+class Lion extends Animal{
+  cry(){
+    return 'roar';
+  }
+}
+class Tiger extends Animal{
+  cry(){
+    return 'grrrr';
+  }
+}
